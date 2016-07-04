4087773297fc4aaa2f2df2d5afbf047ee3f4a18b
Teocci <jrfdeft@gmail.com>
2016-07-04 13:57:58 +0900

    Adding Fork me and LICENSE

--- index.php
+++ index.php
@@ -3,12 +3,18 @@
 <head>
     <meta charset='UTF-8'/>
     <link href="css/style.css" rel="stylesheet">
+    <link href="css/ribbons.css" rel="stylesheet" type="text/css" />
+    <!--[if lt IE 9]>
+    <link href="css/ribbons.ie.css" rel="stylesheet" type="text/css" />
+    <![endif]-->
+
 </head>
 <body>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
 
 <script src="js/socket.js"></script>
+<div><a class="github-fork-ribbon right-top" href="https://github.com/teocci/ChatSocket" title="Fork me on GitHub">Fork me on GitHub</a></div>
 
 <div class="header">
     <h1>ChatSocket</h1>
@@ -22,6 +28,13 @@
         <button id="send-btn">Send</button>
     </div>
 </div>
-
+<div class="footer">
+    <p>
+        <a href="https://www.github.com/teocci/LICENSE">MIT License</a>
+        Copyright &copy; 2016
+        <a target="_blank" href="mailto:teocci@daum.net">Jorge Frisancho (teocci)</a>
+        <a target="_blank" href="https://github.com/teocci">Github</a>
+    </p>
+</div>
 </body>
 </html>
